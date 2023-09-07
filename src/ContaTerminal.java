84cfa6eecc9569aca2401536766718bc63bfd09e
Gabriel Barboza <gabriel.personalti@gmail.com>
2023-09-07 20:05:19 -0300

    dio-trilha-java-basico

---
+++ src/ContaTerminal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner tec= new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo=1000.50;
+
+        System.out.println("Por favor, digite o número da agencia: ");
+        agencia= tec.next();
+        System.out.println("Digite seu nome");
+        nomeCliente=tec.next();
+        System.out.println("Digite o numero da conta:");
+        numero=tec.nextInt();
+
+        System.out.println("olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua " +
+                "agencia é: " + agencia + ", conta: " + numero + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
+    }
+}
